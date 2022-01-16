247d67e40c7099097346485a6d94d2ce791ecc3e
Ovidio Perez <oapm10@gmail.com>
2022-01-16 17:54:21 -0400

    Refactor about-skill components to accept props

--- components/AboutComponent.vue
+++ components/AboutComponent.vue
@@ -1,27 +1,18 @@
 <template>
   <div>
     <about-description></about-description>
-    <h2 class="h1">Work experience</h2>
-    <p class="text-muted mb-4">
-      Received overcame oh sensible so at an. Formed do change merely to county
-      it.
-    </p>
+    <h2 class="h1 mb-4">Work experience</h2>
     <div class="row mb-4">
-      <about-skill></about-skill>
-      <about-skill></about-skill>
-      <about-skill></about-skill>
-      <about-skill></about-skill>
-    </div>
-    <h2 class="h1">Fun facts</h2>
-    <p class="text-muted mb-4">
-      Received overcame oh sensible so at an. Formed do change merely to county
-      it.
-    </p>
-    <div class="row text-center align-items-stretch">
-      <about-fun-facts></about-fun-facts>
-      <about-fun-facts></about-fun-facts>
-      <about-fun-facts></about-fun-facts>
-      <about-fun-facts></about-fun-facts>
+      <about-skill
+        skill="Graphic Designer"
+        desc="From social media to flyers. Set the visual tone for brands in digital and print media."
+        svg-icon="#stack-1"
+      ></about-skill>
+      <about-skill
+        skill="Web Designer"
+        desc="Wireframing and prototyping. Create the website structure and the user interface."
+        svg-icon="#laptop-screen-1"
+      ></about-skill>
     </div>
   </div>
 </template>
--- components/about-components/AboutSkill.vue
+++ components/about-components/AboutSkill.vue
@@ -3,22 +3,30 @@
     <div class="d-flex">
       <div class="flex-grow-1">
         <svg class="svg-icon text-primary">
-          <use xlink:href="#stack-1"></use>
+          <use :xlink:href="svgIcon"></use>
         </svg>
       </div>
       <div class="ml-3">
-        <h3 class="h5 mr-3">Graphic Designer</h3>
-        <p class="text-muted mb-2">
-          eceived overcame oh sensible so at an. Formed do change merely to
-          county it. Am separate contempt domestic to to oh. On relation my so
-          addition branched.
-        </p>
+        <h3 class="h5 mr-3">{{ skill }}</h3>
+        <p class="text-muted mb-2">{{ desc }}</p>
       </div>
     </div>
   </div>
 </template>
 <script>
+/* eslint-disable vue/require-default-prop */
 export default {
   name: 'AboutSkill',
+  props: {
+    skill: {
+      type: String,
+    },
+    desc: {
+      type: String,
+    },
+    svgIcon: {
+      type: String,
+    },
+  },
 }
 </script>
