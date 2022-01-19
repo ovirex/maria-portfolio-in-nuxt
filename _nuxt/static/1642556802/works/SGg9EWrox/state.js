339f27b9b158022f0d0530ca1f2b02b71995393b
ovirex <ovirex@users.noreply.github.com>
2022-01-19 02:08:06 +0000

    deploy: 2bc53e55190f48079b8b3f10b97adb4598c3242a

--- _nuxt/static/1642556802/works/SGg9EWrox/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642556802",layout:"CustomLayout",error:a,serverRendered:true,routePath:"\u002Fworks\u002FSGg9EWrox",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0a98e1ae"}}}}(null));
