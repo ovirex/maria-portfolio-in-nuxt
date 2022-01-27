8ee6d177bfa4237af74e2eb9b044b04919298080
ovirex <ovirex@users.noreply.github.com>
2022-01-27 00:01:11 +0000

    deploy: d4a5d86794df4fa4a05d8661baf245eedcc16089

---
+++ _nuxt/static/1643241669/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fworks\u002Fiugx_Cygy","\u002Fart-direction","\u002Fsocial-media-design","\u002Fweb-design","\u002Fabout","\u002Fworks\u002FZwAxYfS5t","\u002Fworks\u002F_eyQilPHf","\u002Fworks\u002FujF2fz1zL","\u002Fworks\u002FcbVB9ltMF","\u002Fworks\u002FSGg9EWrox","\u002Fworks\u002FozDcOK3bO"]})
---
+++ _nuxt/static/1643241669/works/ZwAxYfS5t/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643241669",layout:"CustomLayout",error:a,serverRendered:true,routePath:"\u002Fworks\u002FZwAxYfS5t",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d37b6a01"}}}}(null));
