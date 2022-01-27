e0f63e9dcfd45ac52ad557e0735bd914e1e03ad4
ovirex <ovirex@users.noreply.github.com>
2022-01-27 00:18:17 +0000

    deploy: 9687ee1401aec9987884094943442ff4c2b5d6d3

--- _nuxt/static/1643242344/works/iugx_Cygy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643242344",layout:"CustomLayout",error:a,serverRendered:true,routePath:"\u002Fworks\u002Fiugx_Cygy",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aa08bb12"}}}}(null));
